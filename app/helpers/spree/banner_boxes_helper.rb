cdf75b62c79b2377b5bcee523370e08f7d7b8cf6
Ricardo Franco <ricardo.krieg@gmail.com>
2014-03-13 16:54:35 -0200

    update banner_boxes_helper.rb

--- app/helpers/spree/banner_boxes_helper.rb
+++ app/helpers/spree/banner_boxes_helper.rb
@@ -1,36 +1,44 @@
 module Spree
-  module BannerBoxesHelper
+    module BannerBoxesHelper
+        def insert_banner_box(params={})
+            params[:category] ||= 'home'
+            params[:class] ||= 'banner'
+            params[:style] ||= SpreeBanner::Config[:banner_default_style]
+            params[:list] ||= false
+            params[:random] ||= true
+            params[:limit] ||= 1
 
-    def insert_banner_box(params={})
-      params[:category] ||= "home"
-      params[:class] ||= "banner"
-      params[:style] ||= SpreeBanner::Config[:banner_default_style]
-      params[:list] ||= false
-      banners = Spree::BannerBox.enabled(params[:category]).order(:position)
-      return '' if banners.empty?
-
-      if params[:list]
-        content_tag :ul do
-          banners.map do |ban|
-            content_tag :li, :class => params[:class] do
-              link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
-                src = ban.attachment.url(params[:style].to_sym)
-                image_tag(src, :alt => ban.alt_text.presence || image_alt(src))
-              end
+            banners = if params[:random]
+                Spree::BannerBox.enabled(params[:category]).limit(params[:limit]).random
+            else
+                Spree::BannerBox.enabled(params[:category]).order(:position).limit(params[:limit])
             end
-          end.join.html_safe
+
+            return '' if banners.nil? or banners.empty?
+
+            banners = [banners] unless banners.is_a?(Array)
+
+            if params[:list]
+                content_tag :ul do
+                    banners.map do |ban|
+                        content_tag :li, :class => params[:class] do
+                            link_to (ban.url.blank? ? 'javascript: void(0)' : ban.url) do
+                                src = ban.attachment.url(params[:style].to_sym)
+                                image_tag(src, :alt => ban.alt_text.presence || image_alt(src))
+                            end
+                        end
+                    end.join.html_safe
+                end
+            else
+                banners.map do |ban|
+                    content_tag :div, :class => params[:class] do
+                        link_to (ban.url.blank? ? 'javascript: void(0)' : ban.url) do
+                            src = ban.attachment.url(params[:style].to_sym)
+                            image_tag(ban.attachment.url(params[:style].to_sym), :alt => ban.alt_text.presence || image_alt(src))
+                        end
+                    end
+                end.join.html_safe
+            end
         end
-      else
-        banners.map do |ban|
-          content_tag :div, :class => params[:class] do
-            link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
-              src = ban.attachment.url(params[:style].to_sym)
-              image_tag(ban.attachment.url(params[:style].to_sym), :alt => ban.alt_text.presence || image_alt(src))
-            end
-          end
-        end.join.html_safe
-      end
     end
-
-  end
 end
